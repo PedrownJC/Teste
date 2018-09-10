5f9ff06639f08d3d658c88677d3dc9fa893e0029
vitor115 <vitorravacci@yahoo.com.br>
2018-09-10 14:32:50 -0300

    codigo polinomio

---
+++ polinomio trabalho.py
@@ -0,0 +1,23 @@
+def polinomiar (v):
+    x
+    y
+    z
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+def main ():
+    polinomiar(vetor)
+
+    
+    vetor = [4,3,-6,-8,3,4]
+    
